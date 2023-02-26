7039cd51f8864208418431c2198382fe9c8597a6
ekaterina.gorshunova <ekaterina.gorshunova@intexsoft.by>
2023-02-26 14:23:59 +0300

    added new credit app type

---
+++ modules/core/db/update/postgres/23/02/230226-4-1411-insertNewDocKind-CreditApplication.sql
@@ -0,0 +1,43 @@
+-- begin insert docKind for CreditApplication
+CREATE OR REPLACE FUNCTION baseInsert()
+RETURNS integer
+AS $$
+DECLARE
+    cnt integer = 0;
+BEGIN
+    cnt = (select count(CATEGORY_ID) from DF_DOC_KIND where CATEGORY_ID = 'f46d5dc5-ffab-6d6e-871d-6b7a11739a28');
+    if (cnt = 0) then
+        insert into SYS_CATEGORY (ID, NAME, ENTITY_TYPE, IS_DEFAULT, CREATE_TS, CREATED_BY, VERSION, DISCRIMINATOR)
+        values ('f46d5dc5-ffab-6d6e-871d-6b7a11739a28', 'Новая заявка на кредит', 'credit$CreditApplication', false, now(), 'system', 1, 1);
+
+        insert into DF_DOC_KIND (CATEGORY_ID, CREATE_TS, CREATED_BY, VERSION, DOC_TYPE_ID, NUMERATOR_ID,
+                                 NUMERATOR_TYPE, CATEGORY_ATTRS_PLACE, TAB_NAME, PORTAL_PUBLISH_ALLOWED, DISABLE_ADD_PROCESS_ACTORS, CREATE_ONLY_BY_TEMPLATE, fields_xml)
+        values ('f46d5dc5-ffab-6d6e-871d-6b7a11739a28', now(), 'system', 1, '25ec4603-3619-4ca8-a3d6-e407894db769', '3cb8e525-c52b-4866-851f-c7af014f5b13',
+                1, 1, 'Доп. поля', false, false, false,
+'<?xml version="1.0" encoding="UTF-8"?>
+<fields>
+  <field name="date" inDocKind="true" required="false" visible="true" signed="false" />
+  <field name="docCategory" inDocKind="true" required="false" visible="true" signed="false" />
+  <field name="owner" inDocKind="true" required="false" visible="true" signed="false" />
+  <field name="department" inDocKind="true" required="false" visible="true" signed="false" />
+  <field name="comment" inDocKind="true" required="false" visible="true" signed="false" />
+  <field name="finishDatePlan" inDocKind="true" required="false" visible="true" signed="false" />
+  <field name="resolution" inDocKind="true" required="false" visible="true" signed="false" />
+  <field name="number" inDocKind="true" required="false" visible="true" signed="false" />
+  <field name="organization" inDocKind="true" required="false" visible="true" signed="false" />
+  <field name="parentCard" inDocKind="true" required="false" visible="true" signed="false" />
+  <field name="theme" inDocKind="true" required="false" visible="true" signed="false" />
+  <field name="credit" inDocKind="true" required="TRUE" visible="true" signed="false" />
+  <field name="individual" inDocKind="true" required="TRUE" visible="true" signed="false" />
+</fields>
+'
+);
+end if;
+return 0;
+END;
+$$
+LANGUAGE plpgsql;
+^
+select baseInsert();^
+drop function if exists baseInsert();^
+-- end insert docKind for CreditApplication
