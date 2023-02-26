87d5d19d1608a26586bbea1d0f063f556df56798
ekaterina.gorshunova <ekaterina.gorshunova@intexsoft.by>
2023-02-26 11:37:38 +0300

    Initial commit

---
+++ modules/global/src/com/company/credit/entity/CreditType.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2023 LTD Haulmont Samara. All Rights Reserved.
+ * Haulmont Samara proprietary and confidential.
+ * Use is subject to license terms.
+ */
+
+package com.company.credit.entity;
+
+import com.haulmont.cuba.core.entity.annotation.EnableRestore;
+import com.haulmont.cuba.core.entity.annotation.TrackEditScreenHistory;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+@Table(name = "CREDIT_CREDIT_TYPE")
+@Entity(name = "credit$CreditType")
+@EnableRestore
+@TrackEditScreenHistory
+public class CreditType extends StandardEntity {
+
+    private static final long serialVersionUID = -7199062103614494819L;
+    @Column(name = "CODE", nullable = false, length = 9)
+    protected String code;
+    @Column(name = "NAME", nullable = false, length = 150)
+    protected String name;
+    @Column(name = "COMMENT_", length = 50)
+    protected String comment;
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
---
+++ modules/web/src/com/company/credit/web/ui/credittype/CreditTypeEdit.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2023 LTD Haulmont Samara. All Rights Reserved.
+ * Haulmont Samara proprietary and confidential.
+ * Use is subject to license terms.
+ */
+
+package com.company.credit.web.ui.credittype;
+
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.company.credit.entity.CreditType;
+import java.util.Map;
+
+
+public class CreditTypeEdit<T extends CreditType> extends AbstractEditor<T> {
+    @Override
+    public void init(Map<String, Object> params) {
+        super.init(params);
+        
+    }
+}
