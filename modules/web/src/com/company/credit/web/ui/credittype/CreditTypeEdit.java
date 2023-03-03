078075dda27f6d7b074ddbaa6a6a10243df9d63d
ekaterina.gorshunova <ekaterina.gorshunova@intexsoft.by>
2023-03-03 14:29:06 +0300

    added uniquenumberservice for autogeneration code value

--- modules/web/src/com/company/credit/web/ui/credittype/CreditTypeEdit.java
+++ modules/web/src/com/company/credit/web/ui/credittype/CreditTypeEdit.java
@@ -6,15 +6,26 @@
 
 package com.company.credit.web.ui.credittype;
 
-import com.haulmont.cuba.gui.components.AbstractEditor;
 import com.company.credit.entity.CreditType;
+import com.haulmont.cuba.core.app.UniqueNumbersService;
+import com.haulmont.cuba.gui.components.AbstractEditor;
+
+import javax.inject.Inject;
 import java.util.Map;
 
 
 public class CreditTypeEdit<T extends CreditType> extends AbstractEditor<T> {
+    @Inject
+    private UniqueNumbersService uniqueNumbersService;
+
     @Override
     public void init(Map<String, Object> params) {
         super.init(params);
-        
+    }
+
+    @Override
+    protected void initNewItem(T code) {
+        super.initNewItem(code);
+        code.setCode(String.valueOf(uniqueNumbersService.getNextNumber(this.getClass().getSimpleName())));
     }
 }
